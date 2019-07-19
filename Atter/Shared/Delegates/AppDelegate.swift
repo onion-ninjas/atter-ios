accb4cc6ef0b25ed129a2584ecccfdaa82a8c5cf
Patryk Mieszała <patryk.mieszala@gmail.com>
2019-07-19 10:09:22 +0200

    project init + cocoapods + swift lint

---
+++ Atter/Shared/Delegates/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Atter
+//
+//  Created by Patryk Mieszała on 19/07/2019.
+//  Copyright © 2019 Patryk Mieszała. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '12.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+project 'Atter'
+
+def shared_pods
+  pod 'SwiftLint', '0.34.0'
+end
+
+target :'Atter' do
+  shared_pods
+end
