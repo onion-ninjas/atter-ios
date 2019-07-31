af30c4812939a1cd65f5b6da40976262bccc6d8e
Patryk Mieszała <patryk.mieszala@gmail.com>
2019-07-31 09:03:02 +0200

    submodule + cleanup

--- Atter/Shared/Delegates/AppDelegate.swift
+++ Atter/Shared/Delegates/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var rootRouter: RootRouter?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         let window = UIWindow()
         window.makeKeyAndVisible()
